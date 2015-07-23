cd037b3fc2f44ce5960b5d30b8cc812ad1781c09
Hans Chan <chenhc@ucweb.com>
2015-07-23 15:10:45 +0800

    add umd

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-'use strict';
-
-var IMG_TYPE_PNG = 'png';
-var IMG_TYPE = [IMG_TYPE_PNG, 'gif', 'bmp', 'jpeg', 'webp'];
-
-for (var i = 0; i < IMG_TYPE.length; i++) {
-  exports[IMG_TYPE[i].toUpperCase()] = IMG_TYPE[i];
-}
-
-exports.resize = function resize(img, width, height, type) {
-  if (!img || !width) {
-    return img;
-  }
-  if (IMG_TYPE.indexOf(type) < 0) {
-    type = IMG_TYPE_PNG;
-  }
-  height = height || width;
-  // 按原图缩放
-  var detImg = img.width / img.height;
-  if (width / height > detImg) {
-    height = width / detImg;
+(function(root, factory) {
+  'use strict';
+  console.log(exports);
+  if (typeof exports === 'object') {
+    // CommonJS
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(function() {
+      return factory();
+    });
+  } else if (typeof define === 'function' && define.cmd) {
+    // CMD
+    define(function(require, exports, module) {
+      module.exports = factory();
+    });
   } else {
-    width = height * detImg;
+    // Global Variables
+    root.ResizeImage = factory();
   }
-  // 画到 canvas 中
-  var canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  var ctx = canvas.getContext('2d');
-  if (type !== IMG_TYPE_PNG) {
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0, 0, width, height);
+}(this, function () {
+  'use strict';
+  var out = {};
+
+  var IMG_TYPE_PNG = 'png';
+  var IMG_TYPE = [IMG_TYPE_PNG, 'gif', 'bmp', 'jpeg', 'webp'];
+
+  for (var i = 0; i < IMG_TYPE.length; i++) {
+    out[IMG_TYPE[i].toUpperCase()] = IMG_TYPE[i];
   }
-  ctx.drawImage(img, 0, 0, width, height);
-  return canvas.toDataURL('image/' + type);
-};
+
+  out.resize = function resize(img, width, height, type) {
+    if (!img || !width) {
+      return img;
+    }
+    if (IMG_TYPE.indexOf(type) < 0) {
+      type = IMG_TYPE_PNG;
+    }
+    height = height || width;
+    // 按原图缩放
+    var detImg = img.width / img.height;
+    if (width / height > detImg) {
+      height = width / detImg;
+    } else {
+      width = height * detImg;
+    }
+    // 画到 canvas 中
+    var canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext('2d');
+    if (type !== IMG_TYPE_PNG) {
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, width, height);
+    }
+    ctx.drawImage(img, 0, 0, width, height);
+    return canvas.toDataURL('image/' + type);
+  };
+
+  return out;
+}));
